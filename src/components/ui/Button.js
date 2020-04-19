37a222656c0b7b63f6dab0846f61ae45aa00cb54
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-19 17:51:00 -0400

    Fix button styles

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,9 +1,9 @@
 import styled, { css } from 'styled-components';
 
 const ButtonStyles = css`
-  background-color: ${props => props.theme.nighttimeBackground};
-  border: 1px solid ${props => props.theme.daytimeBackground};
-  color: ${props => props.theme.nighttimeBackground};
+  background-color: ${props => props.theme.nightBackground};
+  border: 1px solid ${props => props.theme.dayBackground};
+  color: ${props => props.theme.dayBackground};
   font-size: 1rem;
   font-family: sans-serif;
   padding: 0.5rem 1rem;
